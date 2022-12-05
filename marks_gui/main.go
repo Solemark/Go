37076e119dfb46861ed07a5e5ddc658aea7f46dd
Solemarc <62088534+Solemark@users.noreply.github.com>
2022-12-05 19:09:30 +1000

    Add files via upload

---
+++ marks_gui/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"strconv"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Student struct{
+	name string
+	mark int
+}
+
+func get_grade(mark int) string{
+	switch {
+		case mark < 50:
+			return "F"
+		case mark < 65:
+			return "P"
+		case mark < 75:
+			return "C"
+		case mark < 85:
+			return "D"
+		default:
+			return "HD"
+	}
+}
+
+func main() {
+	app := app.New()
+	window := app.NewWindow("Marks GUI")
+
+	var students = []Student{}
+	name_input := widget.NewEntry()
+	name_input.SetPlaceHolder("Name")
+	mark_input := widget.NewEntry()
+	mark_input.SetPlaceHolder("Mark")
+	output := binding.NewString()
+
+	s_output := ""
+	output.Set(s_output)
+	
+	save_button := widget.NewButton("Save", func(){
+		mark, _ := strconv.Atoi(mark_input.Text)
+		student := Student{name_input.Text, mark}
+		students = append(students, student)
+		name_input.SetText("")
+		mark_input.SetText("")
+	})
+	display_button := widget.NewButton("Display", func(){
+		s_output = ""
+		output.Set(s_output)
+		for i := 0; i < len(students); i++ {
+			s_output += students[i].name + " " + get_grade(students[i].mark) + "\n"
+		}
+		output.Set(s_output)
+	})
+	input_container := container.New(layout.NewGridLayout(2), name_input, mark_input)
+	button_container := container.NewHBox(save_button, display_button)
+	container := container.NewVBox(input_container, button_container, widget.NewLabelWithData(output))
+	window.SetContent(container)
+	window.ShowAndRun()
+}
