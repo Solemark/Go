6437c5b3fdea565d4130f9ebc191b6e3527ab0d4
Solemarc <62088534+Solemark@users.noreply.github.com>
2022-12-06 22:36:28 +1000

    Update convert_distance_test.go

--- convert_distance/convert_distance_test.go
+++ convert_distance/convert_distance_test.go
@@ -10,5 +10,14 @@ func Test_convert_distance_10km_to_miles(t *testing.T){
 	if answer != 6.213712 {
 		t.Errorf("convert_distance(%g, %g) = 6.213712, recieved: %g", km, ktm, answer)
 	}
+}
 
+func Test_convert_distance_10_miles_to_km(t *testing.T){
+	m := 10.0
+	mtk := 1.609344
+	answer := convert_distance(m, mtk)
+	
+	if answer != 16.09344 {
+		t.Errorf("convert_distance(%g, %g) = 16.09344, recieved: %g", m, mtk, answer)
+	}
 }
